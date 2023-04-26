e185874b24d4528d660d772a064c42f67770faeb
Oleksii Zasiedka <o.zasiedka@helsi.me>
2023-04-26 19:51:53 +0300

    improved sh

--- script.sh
+++ script.sh
@@ -1,10 +1,28 @@
 #!/usr/bin/bash
 
-echo "O1"
+echo "start - O1"
 
 g++ -std=c++11 -O1 program.cpp -o program_O1
 
 time for i in {1..10}
 do
     ./program_O1
+done
+
+echo "start - O2"
+
+g++ -std=c++11 -O2 program.cpp -o program_O2
+
+time for i in {1..10}
+do
+    ./program_O2
+done
+
+echo "start - O3"
+
+g++ -std=c++11 -O3 program.cpp -o program_O3
+
+time for i in {1..10}
+do
+    ./program_O3
 done
