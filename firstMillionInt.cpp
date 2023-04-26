90ff3ad94295b4f6f1fcb438bc735edb3937d1a0
Oleksii Zasiedka <o.zasiedka@helsi.me>
2023-04-26 18:52:52 +0300

    frhyufgruif

---
+++ firstMillionInt.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <chrono>
+
+int main()
+{
+    const int n = 1000000000;
+    int sum = 0;
+
+    auto start = std::chrono::high_resolution_clock::now();
+
+    for (int i = 1; i <= n; i++)
+    {
+        sum += i;
+    }
+
+    auto end = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> elapsed = end - start;
+
+    std::cout << "Sum: " << sum << std::endl;
+    std::cout << "Elapsed time: " << elapsed.count() << " seconds" << std::endl;
+
+    return 0;
+}
